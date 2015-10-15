0d6301ac080ea0b0d8a786415b1e4f08a8156022
Lingampalli56 <Klingampalli@umail.ucsb.edu>
2015-10-14 21:59:00 -0700

    worked
    worked on stuff

--- LinkedList.h
+++ LinkedList.h
@@ -1,27 +1,27 @@
 #ifndef SOCIALNETWORKPROJECT_LINKEDLIST_H
 #define SOCIALNETWORKPROJECT_LINKEDLIST_H
 
-#include "Node.h";
+#include "Node.h"
 
 template <class X>
 class LinkedList{
 public:
-    Node<X> * head;        // pointer initialization
-    Node<X> * tail;        //pointer initialization
+
 
     LinkedList() ;
     ~LinkedList() ;
 
-    void addElementToEnd(Node<X> *start, X val);
+    void addElementToEnd(X val);
 
-    void removeElement(Node<X> *start, X val);
+    void removeElement(Node<X> val);
 
     void print();
 
-    X retelematNode2D(Node<X> *pointertothenode); // HELP this function returns the data you want from a node in a list
+    //X retelematNode2D(Node<X> *pointertothenode); // HELP this function returns the data you want from a node in a list
 
 
-private:
+    Node<X> * head;
+    Node<X> * tail;
 
 };
 
