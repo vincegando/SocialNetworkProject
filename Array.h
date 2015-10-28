c612d3501fe7b0864d9b91b2e4bf315bbea0bd54
Lingampalli56 <Klingampalli@umail.ucsb.edu>
2015-10-27 22:56:16 -0700

    Implemented array
    Completed with Hemanth
    
    -WOOHOO

--- Array.h
+++ Array.h
@@ -5,6 +5,7 @@
 #ifndef SOCIALNETWORKPROJECT_ARRAY_H
 #define SOCIALNETWORKPROJECT_ARRAY_H
 
+#include <cstddef>
 
 template <typename X>
 class Array {
@@ -13,18 +14,16 @@ public:
     Array();
     ~Array();
     int getSize();
-    void setSize(int s);
     int getCapacity();
-    void setCapacity(int c);
     void insert (int pos, const X & item);
     void remove (int pos);
     void set (int pos, const X & item);
     X const & get (int pos) const;
-    int check();
+    void check();
 
 
 private:
-    int[] list;
+    X* list;
     int size;
     int capacity;
 
@@ -32,62 +31,81 @@ private:
 };
 
 template <typename X>
-Array::Array() {
-    list = int[50];
+Array<X>::Array() {
+    list = new X[50];
     size = 0;
     capacity = 50;
 }
 
 template <typename X>
-Array::~Array() {
-
+Array<X>::~Array() {
+    delete list;
 }
 
 template <typename X>
-int Array::getSize() {
+int Array<X>::getSize() {
     return size;
 }
-
 template <typename X>
-void Array::setSize(int s) {
-    size = s;
-}
-
-template <typename X>
-int Array::getCapacity() {
+int Array<X>::getCapacity() {
     return capacity;
 }
 
 template <typename X>
-void Array::setCapacity(int c) {
-    capacity = c;
+void Array<X>::insert(int pos, const X &item) {
+    //check if position is between 0 and (capacity -1)
+    // array[position] = item
+    //increment size ++
+    // call check()
+
+    pos--;
+    if (pos >=0 && pos < capacity) {
+        if(list[pos]){                   //this checks if some value is already at this position
+           for (int i= size-1; i > pos; i --){
+             list[i+1] = list[i];
+            }
+        }
+        list[pos] = item;        //easy shit
+        size++;
+        check();
+    }
+    return;
 }
 
 template <typename X>
-void Array::insert(int pos, const X &item) {
-
-
+void Array<X>::remove(int pos) {
+    pos--;
+    if(pos >=0 && pos < capacity)
+    {
+        for(int i=pos; i<size-1; i++){
+            list[pos]= list[pos+1];
+        }
+        list[size-1] =NULL;       // cant leave a copy
+    }
+    return;
 }
 
 template <typename X>
-void Array::remove(int pos) {
-
-
+void Array<X>::set(int pos, const X &item) {
+    list[pos]= item;
+    return;
 }
 
 template <typename X>
-void Array::set(int pos, const X &item) {
-
+X const& Array<X>::get(int pos) const {
+    return list[pos];
 }
 
 template <typename X>
-X const& Array::get(int pos) const {
-
-}
-
-template <typename X>
-int Array::check() {
-
+void Array<X>::check() {
+    capacity= 2*capacity;
+    X* newArray = new X[capacity];
+    for(int i=0; i<size; i++)
+    {
+        newArray[i] = list[i];
+    }
+    delete list;
+    list= newArray;
 }
 
 #endif //SOCIALNETWORKPROJECT_ARRAY_H
