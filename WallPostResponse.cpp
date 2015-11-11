77389223335722c62874250d6bdebb6911cbcd12
Lingampalli56 <Klingampalli@umail.ucsb.edu>
2015-11-10 22:07:44 -0800

    add response class -vince

---
+++ WallPostResponse.cpp
@@ -0,0 +1,7 @@
+//
+// Created by Krishna Lingampalli on 11/10/15.
+//
+
+#include "WallPostResponse.h"
+
+
---
+++ WallPostResponse.h
@@ -0,0 +1,20 @@
+//
+// Created by Krishna Lingampalli on 11/10/15.
+//
+
+#ifndef SOCIALNETWORKPROJECT_WALLPOSTRESPONSE_H
+#define SOCIALNETWORKPROJECT_WALLPOSTRESPONSE_H
+
+#include <string>
+
+class WallPostResponse {
+
+    WallPostResponse();
+    WallPostResponse(string comment);
+    ~WallPostResponse();
+    
+
+};
+
+
+#endif //SOCIALNETWORKPROJECT_WALLPOSTRESPONSE_H
